86ce366a218d4860e8e912284a8b89a23586c527
VictorJoelViejoAlvarez <v.j.viejo@udc.es>
2025-02-22 22:45:24 +0100

    A

--- src/backend/models.py
+++ src/backend/models.py
@@ -8,3 +8,27 @@ class Persona(SQLModel, table=True):
     empleado: bool
     telefono: str
     email: str
+
+class Categoria(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    categoria: str
+
+class Competencia(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    competencia: str
+    categoria_id: int
+
+class CompetenciaEmpleado(SQLModel, table=True):
+    empleado_id: int = Field(primary_key=True)
+    competencia_id: int = Field(primary_key=True)
+    obtencion: str
+    verificado: bool
+
+class Problema(SQLModel, table=true):
+    id: int = Field(primary_key=True)
+    problema: str
+    descripcion: str
+    competencia_id: int
+
+class Solucion(SQLModel, table=true):
+    id: int = Field(primary)
